d5b1d813e97f4791025cda9951a0ec79ec82615d
LIKHITH U <88785262+likhigowda@users.noreply.github.com>
2022-08-14 13:24:27 +0530

    Add files via upload

---
+++ jkff to dff/dff_test.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   13:15:40 08/14/2022
+// Design Name:   dff
+// Module Name:   dff_test.v
+// Project Name:  sample
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: dff
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module dff_test_v;
+
+	// Inputs
+	reg d;
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire q;
+
+	// Instantiate the Unit Under Test (UUT)
+	dff uut (
+		.d(d), 
+		.clk(clk), 
+		.rst(rst), 
+		.q(q)
+	);
+
+	initial begin
+		// Initialize Inputs
+		d = 0;
+		clk = 0;
+		rst = 1;
+		
+		#10 rst = 0;
+		#10 d = 1;
+		#10 d = 0;
+		#20 $finish;
+
+	end
+	
+	always #5 clk = ~clk;
+      
+endmodule
+
---
+++ jkff to dff/jkff.v
@@ -0,0 +1,47 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:48:43 08/14/2022 
+// Design Name: 
+// Module Name:    jkff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module jkff(j, k, clk, rst, q);
+    input j;
+    input k;
+    input clk;
+    input rst;
+	 output q;
+	 
+	 reg q;
+	 
+	 always @(posedge clk)
+	 begin
+	 
+		 if(rst)
+			q = 1'b0;
+		 else
+		 begin
+			case({j,k})
+				2'b00:q = q;
+				2'b01:q = 1'b0;
+				2'b10:q = 1'b1;
+				2'b11:q = ~q;
+			endcase
+		 end
+		 
+	 end
+	
+endmodule
