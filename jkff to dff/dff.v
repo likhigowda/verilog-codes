402415f28ee9d23bca4fbc1b1edf4083b251b237
LIKHITH U <88785262+likhigowda@users.noreply.github.com>
2022-08-14 13:27:08 +0530

    Add files via upload

---
+++ jkff to dff/dff.v
@@ -0,0 +1,29 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:58:43 08/14/2022 
+// Design Name: 
+// Module Name:    dff 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module dff(d, clk, rst, q);
+    input d;
+    input clk;
+    input rst;
+    output q;
+	 
+	 jkff u1(d, ~d, clk, rst, q);
+	
+endmodule
