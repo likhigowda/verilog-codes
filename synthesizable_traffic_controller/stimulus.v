3210655351a3c7e7e27e28baad65e2d0af4b971b
LIKHITH U <88785262+likhigowda@users.noreply.github.com>
2022-08-23 19:20:56 +0530

    Add files via upload

---
+++ synthesizable_traffic_controller/stimulus.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module stimulus_v;
+
+// Inputs
+reg clk;
+reg rst;
+reg sensor;
+
+// Outputs
+wire [1:0] state;
+
+// Instantiate the Unit Under Test (UUT)
+traffic_controller uut(clk, rst, sensor, state);
+
+// set clock
+initial clk = 0;
+always #5 clk = ~clk;
+
+// stimulus 
+initial
+begin
+	$monitor($time, " reset = %d, sensor = %d, state = %d",rst,sensor,state);
+	rst = 1;
+	#10 rst = 0;
+	#10 sensor = 1;
+	#40 sensor = 0;
+	#40 $finish;
+end
+
+endmodule
+	
---
+++ synthesizable_traffic_controller/traffic_controller.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+/*
+	hwy	cntry
+s0	green	red
+s1	yellow	yellow
+s2	red	green
+
+*/
+module traffic_controller(clk, rst, sensor, state);
+
+input clk, rst, sensor;
+output [1:0]state;
+
+reg [1:0]state;
+reg [1:0]present_state;
+reg [1:0]next_state;
+
+// constants
+parameter s0 = 2'b00;
+parameter s1 = 2'b01;
+parameter s2 = 2'b10;
+
+// initial values
+initial
+begin
+	present_state <= s0;
+	next_state <= s0;
+end
+
+always @(posedge clk)
+begin
+	if(rst)
+		present_state <= s0;
+	else
+		present_state <= next_state;
+end
+
+always @(present_state)
+begin
+	state <= present_state;
+end
+
+always @(sensor or present_state)
+begin
+	if(sensor == 1)
+	begin
+		case(present_state)
+			s0:next_state = s1;
+			s1:next_state = s2;
+			s2:next_state = s2;
+		endcase
+	end
+	
+	else
+	begin
+		case(present_state)
+			s2:next_state = s1;
+			s1:next_state = s0;
+			s0:next_state = s0;
+		endcase
+	end
+		
+end
+
+endmodule
+	
+	
+		
+	
+
+
